5ff707a7c534aca7eb4059723921ba11bf920f29
Matheus Souza <matheus.ramirez019@gmail.com>
2021-09-26 18:48:07 -0300

    swagger configuration

--- forum/src/main/java/br/com/alura/forum/config/security/SecurityConfig.java
+++ forum/src/main/java/br/com/alura/forum/config/security/SecurityConfig.java
@@ -56,7 +56,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		
+		  web.ignoring()
+	        .antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
 	}
 
 }
